f82a5a53c26a3c0befc0f965ad77b5f780b5f319
Ahsan Ali <ali.ahsan@technado.co>
2021-04-23 11:23:29 +0500

    added_address

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
     originWhitelist: ['http://localhost:3000','http://localhost:50906','http://localhost:53281',
-'https://coinhomes.netlify.app','http://localhost:59579','http://localhost:50000','http://localhost:65446','http://localhost:58058','http://localhost:51518','http://localhost:54844','http://localhost:60003','http://localhost:62571','http://localhost:49976', 'http://localhost:49976','http://localhost:57286','http://localhost:58002','http://localhost:49608'],
+'https://coinhomes.netlify.app','http://localhost:59579','http://localhost:50000','http://localhost:65446','http://localhost:58058','http://localhost:51518','http://localhost:54844','http://localhost:60003','http://localhost:62571','http://localhost:49976', 'http://localhost:49976','http://localhost:57286','http://localhost:58002','http://localhost:49608','http://localhost:58417','http://coinhomes.io'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
