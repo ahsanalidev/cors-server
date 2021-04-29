0cf94bdc921a4450e36944ffd7ac7422273b5b07
Ahsan Ali <ali.ahsan@technado.co>
2021-04-29 14:08:43 +0500

    change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
     originWhitelist: ['http://localhost:3000', 'https://coinhomes.netlify.app', 'https://coinhomes-17f58.firebaseapp.com', 'https://coinhomes.io',
-     'http://localhost:62567'],
+     'http://localhost:63132'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
