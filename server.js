edaca8361f5ba2a03669cabf92a6caca2974d487
Ahsan Ali <ali.ahsan@technado.co>
2021-04-29 15:33:24 +0500

    procy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
     originWhitelist: ['http://localhost:3000', 'https://coinhomes.netlify.app', 'https://coinhomes-17f58.firebaseapp.com', 'https://coinhomes.io',
-     'http://localhost:63817'],
+     'http://localhost:50955'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
